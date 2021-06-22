b30df103148497c188ae2218d93c6fbdcba36215
briancalamar <briansantaran@gmail.com>
2021-06-22 15:07:25 -0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,11 @@ import Temperaments from './components/Temperaments';
 import Cardsort from './components/Cardsort';
 
 
-export default function App({showBar}) {
+export default function App() {
   return (
     <div className="App">
-      {/* {
-      showBar && <NavBar/>
-      } */}
       <Route exact path="/" component={Welcome}/>
-      <Route path="/:homecreate" component={NavBar}/>
-      {/* <Route path="/home" component={Sorts}/>
-      <Route exact path="/home" component={Cards}/> */}
+      <Route path="/" component={NavBar}/>
       <Route exact path="/home" component={Cardsort}/>
       <Route exact path="/createdog" component={CreateDog}/>
       <Route exact path="/home/:name" component={DogDetail}/>
@@ -25,12 +20,3 @@ export default function App({showBar}) {
     </div>
   );
 }
-
-// function mapStateToProps(state) {
-//   return {
-//     showBar: state.infoPage.showBar,
-//   }
-// }
-
-// export default connect(mapStateToProps)(App)
-
--- client/src/components/CardDog.jsx
+++ client/src/components/CardDog.jsx
@@ -31,18 +31,3 @@ export default function CardDog({ name, temperaments, image }) {
         </div>
     )
 }
-
-// return (
-//     <div className="cardDog" style={{background: 'url(' + image + ')'}}>
-//         <h2>{name}</h2>
-//         <div>
-//         <p>Temperaments</p>
-//             <ul>
-//                 {
-//                     temperaments ? temperaments.map((e, i)=> <li key={i} > {e} </li>) :
-//                     <li>No data</li>
-//                 }
-//             </ul>
-//         </div>
-//     </div>
-// )
