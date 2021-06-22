880989447bb2f0ca2dc49a230cc350abf52888b4
briancalamar <briansantaran@gmail.com>
2021-06-22 15:07:00 -0300

    test

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -7,18 +7,45 @@ const { Dog, conn } = require('../../src/db.js');
 const agent = session(app);
 const dog = {
   name: 'Pug',
+  height: "2 - 4",
+  weight: "10 - 20",
+  life_span: "10 - 15 years",
+  image: "image",
 };
 
-describe('Videogame routes', () => {
+
+describe('Dogs routes', () => {
   before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
   beforeEach(() => Dog.sync({ force: true })
     .then(() => Dog.create(dog)));
   describe('GET /dogs', () => {
     it('should get 200', () =>
       agent.get('/dogs').expect(200)
     );
-  });
+    it('should get a object with bd Dogs', async () => {
+      await agent.get('/dogs?ft=true').then((res) => {
+        let dog = res.body.pop()
+        expect(dog).to.deep.equal({
+            id: 1000,
+            name: "Pug",
+            image: "image",
+            temperaments: [],
+            weight: 15
+        })
+    })
+  })
 });
+});
+
+
+
+// it('should get bd dog', () => {
+//   agent.get('/dogs?fs=bd').then((res) => {
+//     expect(res.body).to.be.equal({
+
+//     });
+//   })
+// });
